1c72df064dfe9325562d994bb0f51c9e0b88dc0a
Fischer <E.A.J.Fischer@uu.nl>
2020-03-17 08:23:54 +0100

    Added script for testing and debugging

---
+++ test.R
@@ -0,0 +1,7 @@
+data.one.SIR <- reform.data(cevadata,
+                            sampleday.vars = paste0(c(1:14),".dpch"),
+                            model = "SIR",
+                            positive.if = "max",
+                            inf.rule = 2,
+                            rec.rule = 2,
+                            cut.off = 36)
