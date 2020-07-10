4a173a4e0a074afc8eec339218d42f428cfbd917
Fischer <E.A.J.Fischer@uu.nl>
2020-07-10 08:47:11 +0200

    Major clean up of the repository. I removed all files that are not needed for the supplementary information

--- test.R
+++
@@ -1,8 +0,0 @@
-data.one.SIR <- reform.data(cevadata,
-                            sampleday.vars = paste0(c(1:14),".dpch"),
-                            model = "SIR",
-                            positive.if = "max",
-                            inf.rule = 2,
-                            rec.rule = 2,
-                            cut.off = 36)
-data.one.SIR[[1]]
